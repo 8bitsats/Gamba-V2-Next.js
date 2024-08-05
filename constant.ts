65c2b36d7e8559a218fc3494495923fd8a84927f
8 bit <thesolibrary@gmail.com>
2024-08-05 00:40:53 -0400

    jff

---
+++ constant.ts
@@ -0,0 +1,33 @@
+import { PublicKey } from "@solana/web3.js";
+ 
+// Creator to receive fees
+export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
+  "GzzWXXDjLD4FDwDkWB5sARjC2aaLSfCQDjx3dmpoTY7K",
+);
+ 
+// Fees config
+export const DEFAULT_CREATOR_FEE = 0.05;
+export const DEFAULT_JACKPOT_FEE = 0.01;
+export const DEFAULT_PRIORITY_FEE = 400_201;
+ 
+// Token list
+export const TOKENLIST = [
+  {
+    mint: new PublicKey("So11111111111111111111111111111111111111112"),
+    name: "Solana",
+    symbol: "SOL",
+    image:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png",
+    decimals: 9,
+    baseWager: 0.01e9,
+  },
+  {
+    mint: new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"),
+    name: "USD Coin",
+    symbol: "USDC",
+    image:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png",
+    decimals: 9,
+    baseWager: 0.01e9,
+  },
+];
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,116 +1,45 @@
-// src/pages/_app.tsx
 import "@/styles/globals.css";
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 import {
-  BASE_SEO_CONFIG,
-  LIVE_EVENT_TOAST,
-  PLATFORM_CREATOR_FEE,
-  PLATFORM_JACKPOT_FEE,
-  TOKENLIST,
-} from "../constants";
-import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import { AppProps } from "next/app";
-import { DefaultSeo } from "next-seo";
-import Footer from "@/components/layout/Footer";
-import GameToast from "@/hooks/useGameEvent";
-import Header from "@/components/layout/Header";
-import { Toaster } from "sonner";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { useDisclaimer } from "@/hooks/useDisclaimer";
-import { useMemo } from "react";
-import { useUserStore } from "@/hooks/useUserStore";
-import { PublicKey } from "@solana/web3.js";
-
+import {
+  DEFAULT_CREATOR_FEE,
+  DEFAULT_JACKPOT_FEE,
+  DEFAULT_PRIORITY_FEE,
+  PLATFORM_CREATOR_ADDRESS,
+  TOKENLIST,
+} from "../constants";
 import { GambaPlatformProvider, TokenMetaProvider } from "gamba-react-ui-v2";
 import { GambaProvider, SendTransactionProvider } from "gamba-react-v2";
 
-// Dynamic imports
-// import dynamic from "next/dynamic";
-
-// const GambaPlatformProvider = dynamic(
-//   () => import("gamba-react-ui-v2").then((mod) => mod.GambaPlatformProvider),
-//   { ssr: false }
-// );
-
-// const TokenMetaProvider = dynamic(
-//   () => import("gamba-react-ui-v2").then((mod) => mod.TokenMetaProvider),
-//   { ssr: false }
-// );
-
-// const GambaProvider = dynamic(
-//   () => import("gamba-react-v2").then((mod) => mod.GambaProvider),
-//   { ssr: false }
-// );
-
-// const SendTransactionProvider = dynamic(
-//   () => import("gamba-react-v2").then((mod) => mod.SendTransactionProvider),
-//   { ssr: false }
-// );
+import { AppProps } from "next/app";
+import React from "react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { showDisclaimer, DisclaimerModal } = useDisclaimer();
-  const { isPriorityFeeEnabled, priorityFee, set } = useUserStore((state) => ({
-    isPriorityFeeEnabled: state.isPriorityFeeEnabled,
-    priorityFee: state.priorityFee,
-    set: state.set,
-  }));
-
-  const sendTransactionConfig = isPriorityFeeEnabled ? { priorityFee } : {};
-
-  const network = WalletAdapterNetwork.Mainnet;
-
   const RPC_ENDPOINT =
     process.env.NEXT_PUBLIC_RPC_ENDPOINT ??
-    "https://api.mainnet-beta.solana.com";
-
-  if (!process.env.NEXT_PUBLIC_PLATFORM_CREATOR) {
-    throw new Error(
-      "NEXT_PUBLIC_PLATFORM_CREATOR environment variable is not set"
-    );
-  }
-
-  const PLATFORM_CREATOR_ADDRESS = new PublicKey(
-    process.env.NEXT_PUBLIC_PLATFORM_CREATOR as string
-  );
-
-  const wallets = useMemo(() => [], []);
+    "https://mainnet.helius-rpc.com/?api-key=6b52d42b-5d24-4841-a093-02b0d2cc9fc0";
 
   return (
     <ConnectionProvider
       endpoint={RPC_ENDPOINT}
       config={{ commitment: "processed" }}
     >
-      <WalletProvider autoConnect wallets={wallets}>
+      <WalletProvider autoConnect wallets={[]}>
         <WalletModalProvider>
           <TokenMetaProvider tokens={TOKENLIST}>
-            <SendTransactionProvider {...sendTransactionConfig}>
+            <SendTransactionProvider priorityFee={DEFAULT_PRIORITY_FEE}>
               <GambaProvider>
                 <GambaPlatformProvider
                   creator={PLATFORM_CREATOR_ADDRESS}
-                  defaultCreatorFee={PLATFORM_CREATOR_FEE}
-                  defaultJackpotFee={PLATFORM_JACKPOT_FEE}
+                  defaultCreatorFee={DEFAULT_CREATOR_FEE}
+                  defaultJackpotFee={DEFAULT_JACKPOT_FEE}
                 >
-                  <Header />
-                  <DefaultSeo {...BASE_SEO_CONFIG} />
                   <Component {...pageProps} />
-                  <Footer />
-                  <Toaster
-                    position="bottom-right"
-                    richColors
-                    toastOptions={{
-                      style: {
-                        backgroundImage:
-                          "linear-gradient(to bottom right, #1e3a8a, #6b21a8)",
-                      },
-                    }}
-                  />
-                  {LIVE_EVENT_TOAST && <GameToast />}
-                  {showDisclaimer && <DisclaimerModal />}
                 </GambaPlatformProvider>
               </GambaProvider>
             </SendTransactionProvider>
